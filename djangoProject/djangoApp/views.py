614353ee71ddc5181f281cf8a064769e59b2032c
kanishks201 <kanishks201@gmail.com>
2026-05-09 17:53:33 +0530

    Handle Models & URLs and also created view

--- djangoProject/djangoApp/views.py
+++ djangoProject/djangoApp/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-
+from .models import ChaiVariety
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
 
 def all_chai(request):
-    return render(request, 'djangoApp/all_djangoApp.html')
+    chais = ChaiVariety.objects.all()
+    return render(request, 'djangoApp/all_djangoApp.html', {'chais': chais})
+
+
+def chai_detail(request, chai_id):
+    chai = get_object_or_404(ChaiVariety, pk=chai_id)
+    return render(request, 'djangoApp/chai_detail.html', {'chai': chai})
